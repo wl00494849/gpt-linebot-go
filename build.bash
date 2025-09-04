f8f323503da31e719ba0fa38e7e6b26d9f209d15
zhung1oz7 <wl00494849@gmail.com>
2025-09-04 17:53:48 +0800

    delete DS_Store and add build.bash

---
+++ build.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while getopts "h:u:" opt; do
+    case $opt in
+    h) SERVER=$OPTARG ;;
+    u) USER=$OPTARG ;;
+    esac
+done
+
+docker buildx build --platform linux/arm64 -t line-bot:pi --load .
+docker save line-bot:pi -o linebot_arm64.tar
+scp linebot_arm64.tar ${USER}@${SERVER}:~/image
+rm linebot_arm64.tar
