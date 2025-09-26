6cd7deadecf7f683e1830a8d7ce26ce8839cc6c7
zhung1oz7 <wl00494849@gmail.com>
2025-09-27 01:30:31 +0800

    重構

--- main.go
+++ main.go
@@ -8,16 +8,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	var port string
-	line_secret := os.Getenv("LINE_CHANNEL_SECRET")
-	line_token := os.Getenv("LINE_CHANNEL_TOKEN")
-	python_gpt_url := os.Getenv("PYTHON_GPT_URL")
+var (
+	line_secret    = os.Getenv("LINE_CHANNEL_SECRET")
+	line_token     = os.Getenv("LINE_CHANNEL_TOKEN")
+	python_gpt_url = os.Getenv("PYTHON_GPT_URL")
+	port           string
+)
 
+func init() {
 	flag.StringVar(&port, "p", ":6666", "port")
+	if line_secret == "" || line_token == "" || python_gpt_url == "" {
+		panic("load env fail")
+	}
+}
+
+func main() {
 	lint_bot := src.NewLine_Bot(line_token, line_secret, python_gpt_url)
 
 	app := gin.Default()
+
 	app.POST("/bot_callback", lint_bot.Callback)
 	app.POST("/push_message", lint_bot.Push_Message)
 	app.GET("/live", func(ctx *gin.Context) {
